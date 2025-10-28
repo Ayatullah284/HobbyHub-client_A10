8feacc17bcc91760526933b73d0545cbc9bd8bc4
Ayatullah284 <ayatullah284@gmail.com>
2025-10-28 19:02:16 +0600

    12 commit

--- src/components/AllGroup.jsx
+++ src/components/AllGroup.jsx
@@ -22,7 +22,7 @@ const AllGroups = () => {
       </h2>
 
       {groups.length === 0 ? (
-        <p className="text-center text-gray-500">No groups available</p>
+        <p className="text-center text-gray-500 ">No groups available</p>
       ) : (
         <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
           {groups.map((group) => (
